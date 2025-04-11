9946ea1f58eb223fe5a368bb08ece6b9c746adca
Two-Ocean <51alol456@gmail.com>
2025-04-11 15:13:17 +1200

    Upload

---
+++ ConsoleApp1/ConsoleApp1/Initializers.cs
@@ -0,0 +1,24 @@
+﻿public class Pet
+{
+    public string Name { get; set; } = "";
+    public int Age { get; set; } = 0;
+
+    public Pet() {}
+
+}
+public class Dog : Pet
+{
+    public bool isTrained {  get; set; } = false;
+    public Dog() {}
+}
+public class Cat : Pet
+{
+    public bool isDeclawed { get; set; } = false;
+    public Cat() { }
+}
+
+public class PetOwner
+{
+    public string Name { get; set; } = "";
+    public List<Pet>? Pets { get; set; }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿PetOwner petOwner = new PetOwner
+{
+    Name = "Sia",
+    Pets = new List<Pet> {
+        new Dog { Name = "Sia's Dog", Age= 5, isTrained = true },
+        new Cat { Name = "Sia's Cat", Age = 3, isDeclawed = true}
+    }
+};
+
+Console.WriteLine($"{petOwner.Name}'s Pets");
+foreach(Pet p in petOwner.Pets)
+{
+    Console.WriteLine($"Pet name: {p.Name}, Pet age: {p.Age}");
+    if (p is Dog dog)
+    {
+        Console.WriteLine($"Trained: {dog.isTrained}");
+    }
+    else if (p is Cat cat) 
+    {
+        Console.WriteLine($"Declawed: {cat.isDeclawed}");
+    }
+}
+
+
+Console.ReadLine();
